0d256de5c42816e06efe3449564f0d113de295e1
Abhisek Das <92661441+dasabhisek96@users.noreply.github.com>
2022-11-04 01:34:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dbconfig = require ('./config/db.config');
+
+const auth = require('./middlewares/auth');
+const errors = require('./middlewares/errors');
+
+const unless = require('express-unless');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(
+    () => {
+        console.log('Databas connected');
+    },
+    (error)=> {
+        console.log("Database can`t be connected: " +error );
+    }
+);
+
+auth.authenticateToken.unless = unless;
+app.use(
+    auth.authenticateToken.unless({
+        path: [
+            {url: "/users/login",method:["POST"]},
+            {url: "/users/register",method:["POST"]},
+        ],
+    })
+);
+app.use(express.json());
+
+app.use("/users", require("./routes/users.routes"));
+
+app.use(errors.errorHandler);
+
+app.listen(process.env.post || 4000,function(){
+    console.log("Ready to Go!");
+});
---
+++ users.services.js
@@ -0,0 +1,45 @@
+const User = require("../models/user.model");
+const bcryptjs = require("bcryptjs");
+const { response } = require("express");
+const auth = require("../middlewares/auth.js");
+
+async function login({username, password}, callback){
+    const user = await User.findOne({username});
+
+    if(user != null) {
+        if(bcryptjs.compareSync(password, user.password)){
+            const token = auth.generateAccessToken(username);
+            return callback(null,{...user.toJSON(),token});
+        }
+        else{
+            return callback({
+                message: "Invalid Username/Password!",
+            })
+        }
+    }
+    else{
+        return callback({
+            message:"Invalid Username/Password!",
+        });
+    }
+}
+
+async function register(params, callback){
+    if(params.username === undefined){
+        return callback({message: "Username Required"});
+    }
+
+    const user = new User(params);
+    user.save()
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+module.exports = {
+    login,
+    register,
+};
